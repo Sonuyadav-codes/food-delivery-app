1d9d566d7aa6058cefcd8dbc997cf7785bca5a2d
Sonuyadav-codes <sy8114535@gmail.com>
2026-09-23 10:20:23 +0530

    Add media URL pattern to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('', include('food.urls')),
     path('admin/', admin.site.urls),
+     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
